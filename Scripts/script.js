3311aa8051a564320ec6c1046ed09a146bf561b1
Nitish <tnitish654@gmail.com>
2021-09-16 22:29:33 +0800

    it works! kinda

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,1 +1,23 @@
-const
+const inputList = document.querySelector("#inputList")
+const taskTitle = document.querySelector("#taskTitle")
+const taskInfo = document.querySelector("#taskInfo")
+const addBtn = document.querySelector("#addBtn")
+const taskList = document.querySelector("#taskList")
+
+inputList.addEventListener("submit", onSubmit)
+
+function onSubmit(e) {
+	e.preventDefault()
+
+    console.log("this thing is called")
+
+	const li = document.createElement("li")
+	li.appendChild(
+		document.createTextNode(`${taskTitle.value} \n ${taskInfo.value}`)
+	)
+
+	taskList.appendChild(li)
+
+	taskTitle.value = ""
+	taskInfo.value = ""
+}
